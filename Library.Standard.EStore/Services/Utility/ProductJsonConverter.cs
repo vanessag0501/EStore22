252a4c5f6d0295c07df4affd62e1c2871883d94e
vanessag0501 <63066442+vanessag0501@users.noreply.github.com>
2022-07-25 23:19:27 -0400

    add converters and handler

---
+++ Library.Standard.EStore/Services/Utility/ProductJsonConverter.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.EStore.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Library.Standard.EStore.Utility
+{
+    public class ProductJsonConverter : JsonCreationConverter<Product>
+    {
+        protected override Product Create(Type objectType, JObject jObject)
+        {
+            if (jObject == null) throw new ArgumentNullException("jObject");
+
+            if (jObject["Product By Quantity"] != null || jObject["product by quantity"] != null)
+            {
+                return new ProductByQuantity();
+            }
+            else if (jObject["ProductByWeight"] != null || jObject["product by weight"] != null)
+            {
+                return new ProductByWeight();
+            }
+            else
+            {
+                return new Product();
+            }
+        }
+    }
+}
