2562969e1919df044257b88e5f70b372a9d31f9e
vanessag0501 <63066442+vanessag0501@users.noreply.github.com>
2022-07-29 21:44:11 -0400

    Update ProductByQuantityController.cs

--- EStoreApplication.API/Controllers/ProductByQuantityController.cs
+++ EStoreApplication.API/Controllers/ProductByQuantityController.cs
@@ -1,11 +1,86 @@
-﻿using System;
+using System;
+using EStoreApplication.API.Database;
+using EStoreApplication.API.Database.EC;
+using Library.EStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace EStoreApplication.API.Controllers
 {
-	public class ProductByQuantityController
-	{
-		public ProductByQuantityController()
+	[ApiController]
+	[Route("[controller]")]
+
+
+
+    public class ProductByQuantityController : ControllerBase
+    {
+		private readonly ILogger<ProductByQuantityController> _logger;
+
+		public ProductByQuantityController(ILogger<WeatherForecastController> logger)
 		{
+			_logger = (ILogger<ProductByQuantityController>?)logger;
 		}
+
+
+		[HttpGet]
+		public List<ProductByQuantity> Get()
+		{
+			//return new ProductEC();
+			return eStoreDatabase.productByQuantities;
+		}
+
+		[HttpPost("Create")]
+		public ProductByQuantity Create(ProductByQuantity product)
+        {
+
+			return new ProductEC.Create(product);
+			//new ProductEC().Create();
+			//if (product.Id <= 0)
+			//{
+			//	product.Id = eStoreDatabase.NextId();
+
+
+			//	eStoreDatabase.productByQuantities.Add(product);
+
+			//}
+
+			//var productToUpdate = eStoreDatabase.productByQuantities.FirstOrDefault(t => t.Id == product.Id);
+
+			//if(productToUpdate!=null)
+   //         {
+			//	eStoreDatabase.productByQuantities.Remove(productToUpdate);
+
+			//	eStoreDatabase.productByQuantities.Add(product);
+   //         }
+
+			//return product;
+
+		}
+
+		[HttpGet("Delete/{id}")]
+		public int Delete(int id)
+		{
+			var productToDelete = eStoreDatabase.products.FirstOrDefault(i => i.Id == id);
+
+			if(productToDelete != null)
+            {
+				var product = productToDelete as Product;
+
+				if(product != null)
+                {
+					eStoreDatabase.products.Remove(productToDelete as Product);
+
+				}
+				
+			}
+			
+			return id;
+		}
+
+		
+
+
 	}
+
+
 }
 
