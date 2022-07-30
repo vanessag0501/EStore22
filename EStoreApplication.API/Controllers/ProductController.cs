7c5273518623841841bd87b6aaba4053f290ee02
vanessag0501 <63066442+vanessag0501@users.noreply.github.com>
2022-07-29 21:44:41 -0400

    Update ProductController.cs

--- EStoreApplication.API/Controllers/ProductController.cs
+++ EStoreApplication.API/Controllers/ProductController.cs
@@ -1,11 +1,78 @@
-﻿using System;
+using System;
+using EStoreApplication.API.Database;
+using EStoreApplication.API.Database.EC;
+using Library.EStore.Models;
+using Microsoft.AspNetCore.Mvc;
 namespace EStoreApplication.API.Controllers
 {
-	public class ProductController
+	[ApiController]
+	[Route("[controller]")]
+
+	public class ProductController:ControllerBase
 	{
-		public ProductController()
+		private readonly ILogger<ProductController> _logger;
+
+		public ProductController(ILogger<ProductController> logger)
 		{
+			_logger = (ILogger<ProductController>?)logger;
 		}
+
+
+		[HttpGet]
+		public List<Product> Get()
+		{
+			return Database.eStoreDatabase.products;
+		}
+
+		[HttpPost("Create")]
+		public ProductByQuantity Create(Product product)
+		{
+			if (product.Id <= 0)
+			{
+				product.Id = eStoreDatabase.NextId();
+
+
+				eStoreDatabase.products.Add(product);
+
+			}
+
+			var productToUpdate = eStoreDatabase.products.FirstOrDefault(t => t.Id == product.Id);
+
+			if (productToUpdate != null)
+			{
+				eStoreDatabase.products.Remove(productToUpdate);
+
+				eStoreDatabase.products.Add(product);
+			}
+			
+			return (ProductByQuantity)product;
+
+		}
+
+		[HttpGet("Delete/{id}")]
+		public int Delete(int id)
+		{
+			var productToDelete = eStoreDatabase.products.FirstOrDefault(i => i.Id == id);
+
+			if (productToDelete != null)
+			{
+				var product = productToDelete as Product;
+
+				if (product != null)
+				{
+					eStoreDatabase.products.Remove(productToDelete as Product);
+
+				}
+
+			}
+
+			return id;
+		}
+
+
+
+
 	}
 }
+
 
