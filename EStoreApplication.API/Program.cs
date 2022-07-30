0c5d897590b6ad2a65e81b9bb7e6b5496692d64c
vanessag0501 <63066442+vanessag0501@users.noreply.github.com>
2022-07-29 21:45:27 -0400

    Update Program.cs

--- EStoreApplication.API/Program.cs
+++ EStoreApplication.API/Program.cs
@@ -1,9 +1,17 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(opt => opt.SerializerSettings.Converters.Add(new ProductJsonConverter()));
+
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
